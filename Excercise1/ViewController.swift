ff23c248d284cdf11bb8466f0c5cda74d911bd2e
harp <preetjandu0003@gmail.com>
2020-01-09 15:09:53 -0500

    moved image

--- Excercise1/ViewController.swift
+++ Excercise1/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        image.center = CGPoint(x: image.center.x, y: image.center.y)
+        UIView.animate(withDuration: 3) {
+            self.image.center = CGPoint(x: self.image.center.x, y: self.image.center.y + 710)
+        }
+        
     }
 
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+//    swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+//    view.addGestureRecognizer(swipeRight)
+//
+//    let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+//
+//    swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+//    view.addGestureRecognizer(swipeLeft)
+//
+//}
+//@objc func swiped(gesture: UISwipeGestureRecognizer){
+//  let swipeGesture = gesture as UISwipeGestureRecognizer
+//        switch swipeGesture.direction {
+//        case UISwipeGestureRecognizer.Direction.right:
+//            print("right swipe")
+//
+//        case UISwipeGestureRecognizer.Direction.left:
+//        print("left swipe")
+//        default:
+//            break
+//        }
